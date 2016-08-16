b370c277c35d3430421b0702cb1399612a1c6c8c
Maximilian H <redc0der.mail@gmail.com>
2016-08-16 12:51:34 +0200

    Create info.py

---
+++ info.py
@@ -0,0 +1,16 @@
+hook_files = [
+'',
+]
+
+module_files = [
+'',
+]
+
+target_version = 1.0.0
+
+target_os = ['osx', 'linux', 'windows', 'ios', 'android']
+
+dependencies = {
+'packages': [],
+'python_modules': []
+}
