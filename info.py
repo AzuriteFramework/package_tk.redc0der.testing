770bcda385dafe6b38ff32a73cfabd7eadfd4d31
Maximilian H <redc0der.mail@gmail.com>
2016-08-16 13:27:54 +0200

    Update info.py

--- info.py
+++ info.py
@@ -8,9 +8,9 @@ module_files = [
 
 identifier = 'tk.redc0der.testing'
 
-target_version = '0.5.0'
+target_version = '1.0.0'
 
-target_os = ['osx', 'linux', 'windows', 'ios', 'android']
+target_os = ['osx', 'windows', 'ios', 'android']
 
 dependencies = {
 'packages': [],
